5cdb4f75f378bd02b8c91c805e0c499c86fa0a60
RS Saranyaraj <rs.saranyaraj@gmail.com>
2017-09-11 19:15:03 +0530

    Initial chrome based webdriver orchrastration for WhatsApp messenger
    Changes to be committed:
    	modified:   3_WhatsAppMessenger.py

--- 3_WhatsAppMessenger.py
+++ 3_WhatsAppMessenger.py
@@ -1,1 +1,8 @@
 #!usr/bin/env python3
+
+from selenium import webdriver
+
+driver = webdriver.Chrome("/home/chz/Project/Project-WhatsNews/chromedriver")
+driver.implicitly_wait(5)
+
+driver.get("https://web.whatsapp.com/")
