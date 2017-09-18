71f1b76b3f8b337a49de1e269fb6b639b995545d
RS Saranyaraj <rs.saranyaraj@gmail.com>
2017-09-18 18:28:00 +0530

    Headline and description scraped from The Hindu and printed on the console
    Changes to be committed:
    	modified:   1_NewsScrape.py

--- 1_NewsScrape.py
+++ 1_NewsScrape.py
@@ -6,5 +6,18 @@ from bs4 import BeautifulSoup
 webpage=requests.get("http://www.thehindu.com/")
 print(webpage.status_code)
 soup=BeautifulSoup(webpage.content,"lxml")
-samples=soup.find_all('a')
-print(samples[0])
+samples=soup.find_all("a",href=True)
+
+data={}
+for link in samples:
+	try:
+		soup=BeautifulSoup(requests.get(link['href']).content,"lxml")
+		if soup.find("p",{"class":"s4-3x100-text s4x-100-ls-text hidden-xs hidden-sm hidden-md"}).text==None:
+			continue
+		data["headlines"]=soup.find("div",{"class":"intro"}).text
+		data["description"]=soup.find("p",{"class":"s4-3x100-text s4x-100-ls-text hidden-xs hidden-sm hidden-md"}).text
+		print (data)
+	except KeyboardInterrupt:
+		break
+	except Exception:
+		continue
