b1859616f50fa28fab50a7c937c4001cb9961976
RS Saranyaraj <rs.saranyaraj@gmail.com>
2017-09-18 17:46:35 +0530

    chmod 755 1_NewsScrape.py Tried a code snippet for 3_WhatsAppMessenger.py from http://www.geeksforgeeks.org/whatsapp-using-python/ -> didn't work for now will try the next iteration
    Changes to be committed:
    	modified:   1_NewsScrape.py
    	modified:   3_WhatsAppMessenger.py

--- 3_WhatsAppMessenger.py
+++ 3_WhatsAppMessenger.py
@@ -1,8 +1,30 @@
 #!usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_condition
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 driver = webdriver.Chrome("/home/chz/Project/Project-WhatsNews/chromedriver")
-driver.implicitly_wait(5)
 
 driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+
+target = '"Sanjana Iyyappan"'
+
+msg = time.strftime('%m-%d-%y_%H-%M-%S')
+
+x_arg = '//span[contains(@title,' + target + ')]'
+group_title = wait.until(EC.presence_of_element_located((
+    By.XPATH, x_arg)))
+group_title.click()
+inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
+input_box = wait.until(EC.presence_of_element_located((
+    By.XPATH, inp_xpath)))
+for i in range(100):
+    input_box.send_keys(string + Keys.ENTER)
+    time.sleep(1)
+
+    
