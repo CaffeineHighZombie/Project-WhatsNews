b5bec80bab4151c401d30e9e11981ed657bda70a
RS Saranyaraj <rs.saranyaraj@gmail.com>
2017-09-18 18:10:08 +0530

    Working code -> modified and kinda heavily debugged / jerry-rig version of : https://www.reddit.com/r/programmingrequests/comments/5s7c4f/request_fix_this_selenium_python_whatsapp_web/ https://stackoverflow.com/questions/42046939/whatsapp-web-automation-with-selenium-not-working
    Changes to be committed:
    	modified:   3_WhatsAppMessenger.py

--- 3_WhatsAppMessenger.py
+++ 3_WhatsAppMessenger.py
@@ -1,30 +1,42 @@
 #!usr/bin/env python3
 
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_condition
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome("/home/chz/Project/Project-WhatsNews/chromedriver")
+web = webdriver.Chrome("/home/chz/Project/Project-WhatsNews/chromedriver")
 
-driver.get("https://web.whatsapp.com/")
-wait = WebDriverWait(driver, 600)
+web.get("https://web.whatsapp.com/")
+input('Wait for sometime before pressing enter')
 
-target = '"Sanjana Iyyappan"'
+elem = web.find_element_by_xpath('//span[contains(text(),"Sanjana Iyyappan")]')
+elem.click()
+elem1 = web.find_elements_by_class_name('input')
+elem1[0].send_keys(time.strftime('%m-%d-%y_%H-%M-%S'))
+elem1[0].send_keys('\n')
 
-msg = time.strftime('%m-%d-%y_%H-%M-%S')
+# web.find_element_by_class_name('send-container').click()
 
-x_arg = '//span[contains(@title,' + target + ')]'
-group_title = wait.until(EC.presence_of_element_located((
-    By.XPATH, x_arg)))
-group_title.click()
-inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
-input_box = wait.until(EC.presence_of_element_located((
-    By.XPATH, inp_xpath)))
-for i in range(100):
-    input_box.send_keys(string + Keys.ENTER)
-    time.sleep(1)
+
+
+# wait = WebDriverWait(driver, 600)
+
+# target = '"Sanjana Iyyappan"'
+
+# msg = time.strftime('%m-%d-%y_%H-%M-%S')
+
+# x_arg = '//span[contains(@title,' + target + ')]'
+# group_title = wait.until(EC.presence_of_element_located((
+#     By.XPATH, x_arg)))
+# group_title.click()
+# inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
+# input_box = wait.until(EC.presence_of_element_located((
+#     By.XPATH, inp_xpath)))
+# for i in range(100):
+#     input_box.send_keys(string + Keys.ENTER)
+#     time.sleep(1)
 
     
