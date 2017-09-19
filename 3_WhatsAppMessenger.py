29b7fd03968f0354668d4dd016c0889a4b7a2e26
RS Saranyaraj <rs.saranyaraj@gmail.com>
2017-09-19 19:22:56 +0530

    Refactored the code to be able to import it into other module say RabbitMQ subcribe module and send message on its own. Works for a while the contact_elem = web.find_element_by_xpath('//span[contains(text(), "Sanjana Iyyappan")]') seems to be breaking when adding in the argument contact or even formatting it -> will look into it and then RabbitMQ Publisher and subcription module and add it in the NewsScapper
    Changes to be committed:
    	modified:   3_WhatsAppMessenger.py

--- 3_WhatsAppMessenger.py
+++ 3_WhatsAppMessenger.py
@@ -7,27 +7,31 @@ from selenium import webdriver
 # from selenium.webdriver.common.by import By
 import time
 
-web = webdriver.Chrome("/home/chz/Project/Project-WhatsNews/chromedriver")
+def browser():
+	web = webdriver.Chrome("/home/chz/Project/Project-WhatsNews/chromedriver")
+	web.get("https://web.whatsapp.com/")
+	input('Wait for sometime before pressing enter')
+	return web
 
-web.get("https://web.whatsapp.com/")
-input('Wait for sometime before pressing enter')
+def send(contact, message):
+	web = browser()
+	# contact_elem = web.find_element_by_xpath('//span[contains(text(), {})]'.format(contact))
+	contact_elem = web.find_element_by_xpath('//span[contains(text(), "Sanjana Iyyappan")]')
+	contact_elem.click()
+	input_elem = web.find_elements_by_class_name('input')
+	input_elem[0].send_keys(message)
+	input_elem[0].send_keys('\n')
 
-elem = web.find_element_by_xpath('//span[contains(text(),"Sanjana Iyyappan")]')
-elem.click()
-elem1 = web.find_elements_by_class_name('input')
-elem1[0].send_keys(time.strftime('%m-%d-%y_%H-%M-%S'))
-elem1[0].send_keys('\n')
+if __name__ == '__main__':
+	send("Sanjana Iyyappan", time.strftime('%m-%d-%y_%H-%M-%S'))
+
 
 # web.find_element_by_class_name('send-container').click()
 
 
-
 # wait = WebDriverWait(driver, 600)
-
 # target = '"Sanjana Iyyappan"'
-
 # msg = time.strftime('%m-%d-%y_%H-%M-%S')
-
 # x_arg = '//span[contains(@title,' + target + ')]'
 # group_title = wait.until(EC.presence_of_element_located((
 #     By.XPATH, x_arg)))
