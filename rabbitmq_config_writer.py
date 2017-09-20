2237f976957d981a84e9be4dda4a910e7cde984c
sanjana <sanjuiyyappan@gmail.com>
2017-09-20 19:21:20 +0530

    Add the rabbitMQ configuration file.
     Changes to be committed:
    	new file:   rabbitmq_config.cfg
    	new file:   rabbitmq_config_writer.py

---
+++ rabbitmq_config_writer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import ConfigParser
+
+config = ConfigParser.RawConfigParser()
+
+config.add_section('attributes')
+config.set('attributes', 'host', '192.168.1.190')
+config.set('attributes', 'username', 'user1')
+config.set('attributes', 'password', 'user1')
+
+with open('rabbitmq_config.cfg', 'wb') as configfile:
+    config.write(configfile)
